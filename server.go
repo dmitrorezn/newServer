f53a8198251bf1188a2cf09b8a8285f7659821e6
Дима Резниченко <dmitrorezn@gmail.com>
2020-10-05 15:57:43 +0300

    90%

--- server.go
+++ server.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"ProjectMongoClient"
-	classes "github.com/dmitrorezn/classes"
 	"encoding/json"
 	"fmt"
+	classes "github.com/dmitrorezn/classes"
 	"github.com/gin-gonic/gin"
 	cors "github.com/rs/cors/wrapper/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"io"
+	"os"
 	"strconv"
 	"strings"
-	//"unicode"
+	"time"
 
+	//"unicode"
+	"github.com/gin-gonic/contrib/static"
 	//"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 )
@@ -59,24 +63,51 @@ func RouterGroupsInit(router *gin.Engine) error{
 	//router.LoadHTMLGlob("./css_styles/*")
 	router.LoadHTMLGlob("templ/*")
 	router.Static("/auth/css","./templ")
-
+	router.Static("/auth/script","./templ")
+	router.Use(static.Serve("/photo", static.LocalFile("./photos/", true)))
 	//router.Static("./scripts","C:/Users/dmitr/go/src/newServer/js_codes")
 	router.Use(cors.Default())
 	data := router.Group("/data")
 	{
 		data.Use(CheckUserTokenValidation)
 		data.POST("/show", Show)
+		data.GET("/userlogin",userlogin)
 		data.GET("/announcements", Announcements)
 		data.POST("/announcements", Announcements)
-		data.GET("/activities",OrderActivities)
-		data.POST("/activities",OrderActivities)
+		data.GET("/activities", OrderActivities)
+		//data.POST("/activities", OrderActivities)
+		data.POST("/addToOrder", AddToOrder)
+		data.POST("/delFromOrder", DeleteFromOrder)
+		data.POST("/findAnnouncements", FindAnnouncements)
+
+	}
+	authordata := router.Group("/authordata")
+	{
+		authordata.Use(CheckAuthorTokenValidation)
+		authordata.POST("/show", Show)
+		authordata.GET("/userlogin",userlogin)
+		authordata.GET("/announcements", Announcements)
+		authordata.POST("/announcements", Announcements)
+		authordata.POST("/delAnnouncement",DeleteAnnouncement)
+		authordata.POST("/findAnnouncements", FindAnnouncements)
+
+	}
+	admindata := router.Group("/admindata")
+	{
+		admindata.Use(CheckAdminTokenValidation)
+		admindata.POST("/show", Show)
+		admindata.GET("/userlogin",userlogin)
+		admindata.GET("/announcements", Announcements)
+		admindata.POST("/announcements", Announcements)
+		admindata.POST("/delAnnouncement",DeleteAnnouncement)
+		admindata.POST("/findAnnouncements", FindAnnouncements)
 
 	}
 	auth := router.Group("/auth")
 	{
 		//auth.Use(CheckUserStatus)
-		auth.GET("/announcementinfo",AnnouncementInfo)
-		auth.POST("/announcementinfo",AnnouncementInfo)
+		//auth.GET("/announcementinfo",AnnouncementInfo)
+		//auth.POST("/announcementinfo",AnnouncementInfo)
 		auth.GET("/regform",RegForm)
 		auth.GET("/signform",SignForm)
 		auth.GET("/signup",SignUp)
@@ -86,10 +117,10 @@ func RouterGroupsInit(router *gin.Engine) error{
 	}
 
 
-
 	user := router.Group("/user")
 	{
 		user.Use(CheckUserTokenValidation)
+		user.GET("/logout",Logout)
 		user.GET("/anninfo",AnnInfoHtml)
 		user.POST("/anninfo",AnnInfoHtml)
 		user.GET("/account",Account)
@@ -99,12 +130,21 @@ func RouterGroupsInit(router *gin.Engine) error{
 	}
 	author := router.Group("/author")
 	{
+		author.Use(CheckAuthorTokenValidation)
 		author.GET("/page",AuthorPage)
+		author.GET("/logout",Logout)
 		author.POST("/addannouncement",AddAnnouncement)
+		author.POST("/updateannouncement",UpdateAnnouncement)
+		author.POST("/addImage",AddImage)
 	}
 	admin := router.Group("/admin")
 	{
+		admin.Use(CheckAdminTokenValidation)
 		admin.GET("/page",AdminPage)
+		admin.GET("/logout",Logout)
+		admin.POST("/addannouncement",AddAnnouncement)
+		admin.POST("/updateannouncement",UpdateAnnouncement)
+		admin.POST("/addImage",AddImage)
 	}
 	err := router.Run(":8080")
 	if err != nil {
@@ -121,12 +161,19 @@ func SignForm(c *gin.Context)  {
 	c.HTML(http.StatusOK, "signin.html", nil)
 }
 
+func userlogin(c *gin.Context)  {
+	login , err := c.Cookie("login")
+	if err!= nil{
+		c.String(400,err.Error())
+		return
+	}
+	c.JSON(200,login)
+}
+
 func AnnInfoHtml(c *gin.Context)  {
-	fmt.Println("anninfo.html")
 	var selector map[string]interface{}
 	curAnnId,err := c.Cookie("cur_ann_id")
 	if err != nil {
-		fmt.Println("Account->no cookie ",err.Error())
 		c.String(400,"Account->no cookie ",err.Error())
 		return
 	}
@@ -134,7 +181,6 @@ func AnnInfoHtml(c *gin.Context)  {
 	selector["_idstr"] = curAnnId
 	announcements, err := session.ReadAnnouncements(selector)
 	if err != nil || len(announcements)>1 {
-		fmt.Println("ReadAnnouncements-> err ",err.Error())
 		c.String(400,"ReadAnnouncements-> err ",err.Error())
 	}
 	fmt.Println(announcements[0])
@@ -150,11 +196,9 @@ func Account(c *gin.Context)  {
 	var selector map[string]interface{}
 	announcements,err := session.ReadAnnouncements(selector)
     if err != nil{
-    	fmt.Println(err.Error())
-    	c.String(400,err.Error())
+    	c.String(400,"ReadAnnouncements->"+err.Error())
 		return
 	}
-	fmt.Println(c.Cookie("login"))
 	c.HTML(http.StatusOK, "index.html", announcements)
 }
 
@@ -166,53 +210,91 @@ func Show(c *gin.Context)  {
 	var data interface{}
 	err := c.BindJSON(&data)
 	if err != nil{
-		fmt.Println(err.Error())
-		c.String(400,err.Error())
+		c.String(400,"BindJSON->"+err.Error())
 		return
 	}
 	c.JSON(200, data)
-	fmt.Println("DATA",data)
 }
 
 //middleware token authorisation for user
 func CheckUserTokenValidation(c *gin.Context) {
-	_, err1 := c.Cookie("token")
-	status,err2 := c.Cookie("status")
-	if err1 != nil || err2 != nil || status != "user" {
-		c.HTML(200, "signup.html", gin.H{
+	_, err := c.Cookie("token")
+	if err != nil{
+
+		c.HTML(400, "signup.html", gin.H{
 			"title": "authorisation",
-			"error1":err1.Error(),
-			"error2":err2.Error(),
+			"error":err.Error(),
 		})
+		return
+	}
+	status,err := c.Cookie("status")
+	if err != nil {
+		fmt.Println(err.Error())
+		c.HTML(400, "signup.html", gin.H{
+			"title": "authorisation",
+			"error":err.Error(),
+		})
+		return
+	}
+	if status != "user"{
+		c.String(400,"wrong status")
 		return
 	}
 	return
 }
 
-//middleware token authorisation for user
-//func CheckUserStatus(c *gin.Context) {
-//	status, err := c.Cookie("status")
-//	if err!=nil {
-//		c.HTML(200, "signin.html", gin.H{
-//			"title": "authorisation",
-//		})
-//		return
-//	}
-//	if status == "user"{
-//
-//		return
-//	}
-//	if status == "author"{
-//		return
-//	}
-//	if status =="admin"{
-//		return
-//	}
-//	return
-//}
+func CheckAuthorTokenValidation(c *gin.Context) {
+	_, err := c.Cookie("token")
+	if err != nil{
+		c.HTML(200, "signup.html", gin.H{
+			"title": "authorisation",
+			"error":err.Error(),
+		})
+		return
+	}
+	status,err := c.Cookie("status")
+	if err != nil {
+		c.HTML(200, "signup.html", gin.H{
+			"title": "authorisation",
+			"error":err.Error(),
+		})
+		return
+	}
+	if status != "author"{
+		c.String(400,"wrong status")
+		return
+	}
+	return
+}
+func CheckAdminTokenValidation(c *gin.Context) {
+	fmt.Println("hd")
+	_, err := c.Cookie("token")
+	if err != nil{
+
+		c.HTML(400, "signup.html", gin.H{
+			"title": "authorisation",
+			"error":err.Error(),
+		})
+		return
+	}
+	status,err := c.Cookie("status")
+	if err != nil {
+		fmt.Println(err.Error())
+		c.HTML(400, "signup.html", gin.H{
+			"title": "authorisation",
+			"error":err.Error(),
+		})
+		return
+	}
+	fmt.Println(status)
+	if status != "admin"{
+		c.String(400,"wrong status")
+		return
+	}
+	return
+}
 
 func SignUp(context *gin.Context) {
-
 		login := context.PostForm("login")
 		password := context.PostForm("password")
 		email := context.PostForm("email")
@@ -240,7 +322,6 @@ func SignUp(context *gin.Context) {
 }
 
 func SignIn(context *gin.Context) {
-
 		login,ok1:= context.GetPostForm("login")
 		password,ok2:= context.GetPostForm("password")
 		fmt.Println(login,password)
@@ -257,7 +338,6 @@ func SignIn(context *gin.Context) {
 			context.SetCookie("login", user.Username, 5*60, "/", "localhost", false, false)
 			context.SetCookie("status", user.Status, 5*60, "/", "localhost", false, false)
 			//return HTML
-
 			if user.Status == "user"{
 				Account(context)
 
@@ -278,8 +358,8 @@ func SignIn(context *gin.Context) {
 				return
 			}else {
 				context.String(400,"No status info for user "+user.Username)
+				return
 			}
-			return
 		} else {
 			fmt.Println("no password or email")
 			context.String(http.StatusBadRequest, "no password or email")
@@ -308,13 +388,41 @@ func Trash(c *gin.Context)  {
 
 func Announcements(c *gin.Context)  {
 	var selector = make(map[string]interface{})
-	//var announcements  []classes.Announcement
-	//var err error
-	announcements, err := session.ReadAnnouncements(selector)
-	if err != nil {
+	var announcements  []classes.Announcement
+	login, err := c.Cookie("login")
+	if err !=  nil{
 		fmt.Println(err.Error())
-		c.String(400, err.Error())
+		c.String(400,err.Error())
 		return
+	}
+	status, err := c.Cookie("status")
+	if err !=  nil{
+		fmt.Println(err.Error())
+		c.String(400,err.Error())
+		return
+	}
+	if status == "author" {
+		selector["auth_login"] = login
+		announcements, err = session.ReadAnnouncements(selector)
+		if err != nil {
+			fmt.Println(err.Error())
+			c.String(400, err.Error())
+			return
+		}
+	}else if status == "user"{
+		announcements, err = session.ReadAnnouncements(selector)
+		if err != nil {
+			fmt.Println(err.Error())
+			c.String(400, err.Error())
+			return
+		}
+	}else if status == "admin"{
+		announcements, err = session.ReadAnnouncements(selector)
+		if err != nil {
+			fmt.Println(err.Error())
+			c.String(400, err.Error())
+			return
+		}
 	}
 	out,_ :=json.MarshalIndent(announcements,"  ","   ")
     fmt.Println(announcements[0].Title)
@@ -325,8 +433,6 @@ func Announcements(c *gin.Context)  {
 
 func OrderActivities(c *gin.Context)  {
 	var selector = make(map[string]interface{})
-	//var announcements  []classes.Announcement
-	//var err error
 	login, err := c.Cookie("login")
 	if err != nil{
 		fmt.Println(err.Error())
@@ -343,7 +449,40 @@ func OrderActivities(c *gin.Context)  {
 	out,_ :=json.MarshalIndent(order.ActivityList,"  ","   ")
 	fmt.Println(string(out))
 	c.JSON(200,order.ActivityList)
+	return
+}
 
+func UpdateAnnouncement(c *gin.Context)  {
+	var selector = make(map[string]interface{})
+	var set = make(map[string]interface{})
+	var data map[string]string
+	err := c.BindJSON(&data)
+	if err != nil{
+		fmt.Println(err.Error())
+		return
+	}
+	price, err := strconv.ParseFloat(data["price"],64)
+	if err!=nil{
+		fmt.Println(err.Error())
+		return
+	}
+	selector["_idstr"] = data["idstr"]
+	set["title"] = data["title"]
+	set["activity.name"] = data["name"]
+	set["activity.type"] = data["type"]
+	set["activity.price"] = price
+	set["activity.description"] = data["description"]
+	set["email"] = data["email"]
+	set["phone_number"] = data["phone_number"]
+
+	err = session.Update(selector,set,false,"announcements")
+	if err != nil{
+		fmt.Println(err.Error())
+		c.String(400,err.Error())
+		return
+	}
+	c.JSON(200,nil)
+	return
 }
 
 func AddAnnouncement(c *gin.Context)  {
@@ -368,101 +507,232 @@ func AddAnnouncement(c *gin.Context)  {
 	 	fmt.Println("err no title")
 		 return
 	 }
-
+	 startDates := strings.Split(data["start_dates"]," ")
+	 fmt.Println(startDates)
 	 id := primitive.NewObjectID()
+	 actid := primitive.NewObjectID()
+	 actidstr := strings.Split(actid.String(),"(")[1]
+	 idstr := strings.Split(id.String(),"(")[1]
+	 ids := []rune(idstr)
+	 actids := []rune(actidstr)
+	 ids = ids[1:len(ids)-2]
+	 actids = actids[1:len(actids)-2]
+	 imPath := string(ids)+"_image"
      announcement := classes.Announcement{
      	ID: id,
-     	IDString: id.String(),
+     	IDString: string(ids),
      	Title: title,
+     	ImagePath: imPath,
      	AuthorLogin: login,
      	Email: data["email"],
-     	StartWeekDays: strings.Split(data["start_days"]," "),
+     	StartWeekDays: startDates,
      	PhoneNumber: data["phone_number"],
      	Activity:classes.Activity{
+     		ID: primitive.NewObjectID(),
+     		IDSting: string(actids),
      		Name: data["name"],
      		Type: data["type"],
      		Price: price,
      		Description: data["description"],
 		},
 	 }
+	 impath:= string(ids)+"_image"
+	 c.SetCookie("annImPath",impath, 3600, "/", "localhost", false, false)
 	 err = session.Insert(announcement,"announcements")
      if err != nil{
      	fmt.Println(err.Error())
      	c.String(400,err.Error())
      	return
 	 }
-     fmt.Println(announcement)
-}
-func AnnouncementInfo(c *gin.Context)  {
-	fmt.Println("info")
-	idData := make(map[string]string)
-    err := c.BindJSON(&idData)
-    if err !=nil{
-    	fmt.Println(err.Error())
-    	//c.String(400,err.Error())
-		return
-	}
-    id := idData["idstr"]
-    fmt.Println(id)
-    _, err = c.Cookie("cur_ann_id")
-    if err == nil{
-		c.SetCookie("cur_ann_id", "", -1, "/", "localhost", false, false)
-	}
-	c.SetCookie("cur_ann_id", id, 3600, "/", "localhost", false, false)
-	return
-}
-func AddComment(c *gin.Context)  {
-     var comment classes.Comment
-     err := c.BindJSON(&comment)
-     if err !=nil{
-     	fmt.Println(err.Error())
-     	c.String(400,err.Error())
-		 return
-	 }
-	 err = session.Insert(comment,"coments")
-     if err != nil{
-		 fmt.Println(err.Error())
-		 c.String(400,err.Error())
-		 return
-	 }
-	 return
+	 c.JSON(200,nil)
+     //fmt.Println(announcement)
 }
 
+func AddImage(c *gin.Context)  {
+    imPath,err := c.Cookie("annImPath")
+    if err != nil{
+    	fmt.Println("path cookie"+err.Error())
+    	c.String(400,"path cookie"+err.Error())
+	}
+	c.SetCookie("annImPath", "", -1, "/", "localhost", false, false)
+
+	src, _, err := c.Request.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	defer src.Close()
+	dst, err := os.Create("./photos/" + imPath)
+	if err != nil {
+		c.String(400,err.Error())
+		return
+	}
+	defer dst.Close()
+	_, err = io.Copy(dst, src)
+	if err != nil{
+		c.String(400, "io.Copy()->"+err.Error())
+		return
+	}
+	c.JSON(200,nil)
+	return
+}
+
+
 func AddToOrder(c *gin.Context)  {
-	selector := make(map[string]interface{})
+	var dbannouns []classes.Announcement
+	var dbact classes.Activity
+	activities := make([]classes.Activity,2)
+	info := make(map[string]string)
+	orderselector := make(map[string]interface{})
+	annselector := make(map[string]interface{})
 	change := make(map[string]interface{})
-	var act classes.Activity
-	login,err := c.Cookie("login")
-	if err != nil{
+	err1 :=c.BindJSON(&info)
+	login,err2 := c.Cookie("login")
+	if err1 !=nil || err2 !=nil{
+		fmt.Println("no data id or cookie")
+		c.String(400,"no data id or cookie")
+		return
+	}
+    fmt.Println(info)
+	annselector["_idstr"] = info["idstr"]
+	dbannouns, err := session.ReadAnnouncements(annselector)
+	dbact = dbannouns[0].Activity
+	dbact.StartDate = info["day"]
+	orderselector["user_login"] = login
+	timems := strings.Split(time.Now().String(),".")[1]
+	order, err := session.ReadOrder(orderselector)
+	if err != nil {
+		if err.Error() == "not found" {
+			activities = append(activities, dbact)
+			strid := dbact.IDSting
+			dbact.IDSting = strid +"_"+ strings.Split(timems," ")[0]
+			neworder := classes.Order{
+				UserLogin:    login,
+				ActivityList: activities,
+				TotalPrice:   activities[0].Price,
+			}
+			err := session.Insert(neworder, "orders")
+			if err != nil{
+				fmt.Println(err.Error())
+				c.String(400,err.Error())
+				return
+			}
+			fmt.Println("inserted new order ")
+			c.JSON(200,dbact)
+			return
+		}else{
 		fmt.Println(err.Error())
 		c.String(400,err.Error())
 		return
+		}
 	}
-	selector["user_login"] = login
-	err = c.BindJSON(&act)
-	if err != nil{
-		fmt.Println(err.Error())
-		c.String(400,err.Error())
-		return
-	}
-	order, err := session.ReadOrder(selector)
-	if err != nil{
-		fmt.Println(err.Error())
-		c.String(400,err.Error())
-		return
-	}
-	acts := order.ActivityList
-	acts = append(acts,act)
-	change["$set"]=bson.M{
-		"activity_list": acts,
-	}
-	err = session.Update(selector,change,false,"orders")
+	strid := dbact.IDSting
+	dbact.IDSting = strid +"_"+ strings.Split(timems," ")[0]
+	dbactivities := order.ActivityList
+	dbactivities = append(dbactivities,dbact)
+	change["activity_list"] = dbactivities
+	err = session.Update(orderselector,change,false,"orders")
 	if err != nil{
 		fmt.Println("Update -> err ",err.Error())
 		c.String(400,"Update -> err ",err.Error())
+		return
 	}
 	fmt.Println("added activity")
+	fmt.Println(dbact)
+	acts := []classes.Activity{dbact}
+	c.JSON(200,acts)
+	return
+}
+func Logout(c *gin.Context) {
+	// Clear the cookie
+	c.SetCookie("token", "", -1, "", "", false, true)
+	c.SetCookie("status", "", -1, "", "", false, true)
+	c.SetCookie("login", "", -1, "", "", false, true)
+	c.Set("isLoggedIn", false)
+	// Redirect to the home page
+	c.Redirect(http.StatusFound, "/auth/signform")
+}
+func DeleteFromOrder(c *gin.Context) {
+
+	selector := make(map[string]interface{})
+	set := make(map[string]interface{})
+	login, err := c.Cookie("login")
+	if err != nil{
+		fmt.Println(err.Error())
+		c.String(400,err.Error())
+		return
+	}
+
+	selector["user_login"] = login
+	order, err := session.ReadOrder(selector)
+	actList := order.ActivityList
+	var delId string
+	err = c.BindJSON(&delId)
+	if err != nil {
+		fmt.Println("deleteFromOrder() ->", err.Error())
+		return
+	}
+	for k := range actList {
+		if actList[k].IDSting == delId {
+			actList[k] = actList[len(actList)-1]
+			actList = actList[:len(actList)-1]
+			break
+		}
+	}
+	fmt.Println("delete Activity  ->",  delId)
+	set["activity_list"] = actList
+	err = session.Update(selector,set,false,"orders")
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Println("Removed Activity :",delId )
+	}
+	c.JSON(200,nil)
 }
 
+func DeleteAnnouncement(c *gin.Context)  {
+	selector := make(map[string]interface{})
+	var idstr string
+	err := c.BindJSON(&idstr)
+	if err != nil {
+		c.String(400,err.Error())
+		return
+	}
+	selector["_idstr"] = idstr
+	err = session.Delete(selector,false,"announcements")
+	if err != nil{
+		c.String(400,"Delete->" + err.Error())
+		return
+	}
+	c.JSON(200,nil)
+	return
+}
 
-
+func FindAnnouncements(c *gin.Context)  {
+     titleselector := make(map[string]interface{})
+	 nameselector := make(map[string]interface{})
+	 typeselector := make(map[string]interface{})
+	 var input string
+     var announcements []classes.Announcement
+     err :=  c.BindJSON(&input)
+     if err != nil{
+     	c.String(400,err.Error())
+		 return
+	 }
+	 titleselector["title"] = input
+	 announcements, err = session.ReadAnnouncements(titleselector)
+	 if err != nil{
+		 nameselector["activity.name"] = input
+		 announcements, err = session.ReadAnnouncements(nameselector)
+		 if err != nil{
+			 typeselector["activity.type"] = input
+			 announcements, err = session.ReadAnnouncements(typeselector)
+             if err != nil{
+             	c.JSON(400,nil)
+             	return
+			 }
+		 }
+	 }
+     c.JSON(200,announcements)
+	 return
+}
--- templ/js_scripts.js
+++ templ/js_scripts.js
@@ -1,16 +1,153 @@
 function sendData() {
+    let err = 1
+    if ($('#name').val().toString() == "undefined" || Boolean($('#name').val().toString())==false){
+        $('#name').attr("style","background-color:red")
+        err = 0
+    } else {$('#name').attr("style","background-color:white")}
+    if ($('#title').val().toString() == "undefined" || Boolean($('#title').val().toString())==false){
+        $('#title').attr("style","background-color:red")
+        err = 0
+    } else {$('#title').attr("style","background-color:white")}
+    if ($('#type').val().toString() == "undefined" || Boolean($('#type').val().toString())==false){
+        $('#type').attr("style","background-color:red")
+        err = 0
+    } else {$('#type').attr("style","background-color:white")}
+    if ($('#email').val().toString() == "undefined" || Boolean($('#type').val().toString())==false){
+        $('#email').attr("style","background-color:red")
+        err = 0
+    } else {$('#email').attr("style","background-color:white")}
+    if ($('#price').val().toString() == "undefined" || Boolean(parseFloat($('#price').val().toString()))==false || Boolean($('#price').val().toString())==false){
+        $('#price').attr("style","background-color:red")
+        err = 0
+    } else {$('#price').attr("style","background-color:white")}
+    if ($('#description').val().toString() == "undefined" || Boolean($('#description').val().toString())==false){
+        $('#description').attr("style","background-color:red")
+        err = 0
+    } else {$('#description').attr("style","background-color:white")}
+    if ($('#phone_number').val().toString() == "undefined" || Boolean($('#phone_number').val().toString())==false){
+        $('#phone_number').attr("style","background-color:red")
+        err = 0
+    } else {$('#phone_number').attr("style","background-color:white")}
+    if ($('#start_dates').val().toString() == "undefined" || Boolean($('#start_dates').val().toString())==false) {
+        $('#start_dates').attr("style", "background-color:red")
+        err = 0
+    } else {$('#start_dates').attr("style","background-color:white")}
+    if (err == 0){
+        alert("err")
+        return
+    }
     let data = {
         "name": $('#name').val(),
+        "title":$('#title').val(),
+        "type":$('#type').val(),
+        "price":$('#price').val(),
+        "description":$('#description').val(),
+        "email":$('#email').val(),
+        "phone_number":$('#phone_number').val(),
+        "start_dates":$('#start_dates').val(),
     }
     $.ajax({
-        url: '/data/show',
+        url: '/author/addannouncement',
         type: 'POST',
         data: JSON.stringify(data),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success : function(data) {
-            $('#brd').append("<div>"+ data.name +"</div>");
+                $('#name').val("");
+                $('#title').val("");
+                $('#type').val("");
+                $('#price').val("");
+                $('#description').val("");
+                $('#email').val("");
+                $('#phone_number').val("");
+                $('#start_dates').val("");
+           // $('#lines').append("<div>Title"+data.title+"</div><br>"+"<div>Name"+data.name+"</div><br>")
+            alert("added new announcement reload page!")
         },
+        error:function (){
+            alert("wrong")
+        }
     });
     console.log(data)
 }
+
+
+
+// function removeFromChart(item) {
+//     alert("s")
+//     let it = document.getElementById(item);
+//     it.parentNode.removeChild(it);
+//     let dataId = item.toString()
+    // $.ajax({
+    //     url: "/data/delFromOrder",
+    //     method: "POST",
+    //     data: JSON.stringify(data),
+    //     contentType: "application/json; charset=utf-8",
+    //     dataType: "json",
+    //     success: function (data) {
+    //         $("#chartitems").append("<div id=" + dataId + ">" +
+    //             "<ul class='chartitem' >" +
+    //             "<li><div class ='chartitemimage'></div></li>" +
+    //             "<li><p>Name:" + data.name + "</p></li>" +
+    //             "<li><p>Type:" + data.type + "</p></li>" +
+    //             "<li><p>Date:" + data.start_date + "</p></li>" +
+    //             "</ul>" +
+    //             "<div style='background-color: crimson' onclick=removeFromChart('" + id + "')></div>" +
+    //             "</div>");
+    //     },
+    //     error: function () {
+    //         alert("err err chart add")
+    //     },
+    // })
+// }
+
+// //////////////////////////////////////////
+// function start(d, axios) {
+//     "use strict";
+//     let inputFile = d.querySelector("#inputFile");
+//     let divNotification = d.querySelector("#alert");
+//
+//     inputFile.addEventListener("change", addFile);
+//
+//     function upload(file) {
+//         var formData = new FormData()
+//         formData.append("file", file)
+//         post("/author/addImage", formData)
+//             .then(onResponse)
+//             .catch(onResponse);
+//     }
+//     function onResponse(response) {
+//         var className = (response.status !== 400) ? "success" : "error";
+//         divNotification.innerHTML = response.data;
+//         divNotification.classList.add(className);
+//         setTimeout(function() {
+//             divNotification.classList.remove(className);
+//         }, 3000);
+//     }
+//     function addFile(e) {
+//         var file = e.target.files[0]
+//         if(!file){
+//             return
+//         }
+//         upload(file);
+//     }
+//     setTimeout(function() {
+//         divNotification.classList.remove(className);
+//     }, 3000);
+// }
+//
+// function onResponse(response) {
+//     var className = (response.status !== 400) ? "success" : "error";
+//     divNotification.innerHTML = response.data;
+//     divNotification.classList.add(className);
+//
+// }
+//
+// function post(url, data) {
+//     return axios.post(url, data)
+//         .then(function (response) {
+//             return response;
+//         }).catch(function (error) {
+//             return error.response;
+//         });
+// }
